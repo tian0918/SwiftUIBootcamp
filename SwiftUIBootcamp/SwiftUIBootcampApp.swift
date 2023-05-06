b72d6a2dfd51476f305680eff475104b43bbb27e
txby_iOS <tiany@tianixiabuyi.com>
2023-05-06 14:39:36 +0800

    初始化

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by txby on 2023/5/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
